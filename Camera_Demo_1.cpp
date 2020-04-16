078cfa2cce146e83c4e6c26bc150714a06ca23b1
falcon0172 <rishilshah57@gmail.com>
2020-04-16 10:50:48 +0100

    Add files via upload

---
+++ Camera_Demo_1.cpp
@@ -0,0 +1,475 @@
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#define GLEW_STATIC
+#include "GL/glew.h"	// Important - this header must come before glfw3 header
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "ShaderProgram.h"
+#include "Texture2D.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+// Global Variables
+const char* APP_TITLE = "Normal Map Vs Parallax Map";
+int gWindowWidth = 1024;
+int gWindowHeight = 768;
+GLFWwindow* gWindow = NULL;
+bool gWireframe = false;
+const std::string diffuseMap = "textures/brick_diffuse_map.jpg";
+const std::string normalMap = "textures/brick_normal_map.jpg";
+
+
+const std::string depthMap = "textures/brick_disp_map.jpg";
+glm::vec3 lightPos(0.5f, 1.0f, 0.3f);
+float heightScale = 5;
+
+
+
+// Function prototypes
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void glfw_onFramebufferSize(GLFWwindow* window, int width, int height);
+void showFPS(GLFWwindow* window);
+bool initOpenGL();
+
+//-----------------------------------------------------------------------------
+// Main Application Entry Point
+//-----------------------------------------------------------------------------
+int main()
+{
+	if (!initOpenGL())
+	{
+		// An error occured
+		std::cerr << "GLFW initialization failed" << std::endl;
+		return -1;
+	}
+
+	// positions
+
+	glm::vec3 pos1(-1.0f, 1.0f, 0.0f);
+	glm::vec3 pos2(-1.0f, -1.0f, 0.0f);
+	glm::vec3 pos3(1.0f, -1.0f, 0.0f);
+	glm::vec3 pos4(1.0f, 1.0f, 0.0f);
+
+
+	// texture coordinates
+	glm::vec2 uv1(0.0f, 1.0f);
+	glm::vec2 uv2(0.0f, 0.0f);
+	glm::vec2 uv3(1.0f, 0.0f);
+	glm::vec2 uv4(1.0f, 1.0f);
+	// normal vector
+	glm::vec3 nm(0.0f, 0.0f, .0f);
+
+	// calculate tangent/bitangent vectors of both triangles
+	glm::vec3 tangent1, bitangent1;
+	glm::vec3 tangent2, bitangent2;
+
+	// triangle 1
+	// ----------
+	glm::vec3 edge1 = pos2 - pos1;
+	glm::vec3 edge2 = pos3 - pos1;
+	glm::vec2 deltaUV1 = uv2 - uv1;
+	glm::vec2 deltaUV2 = uv3 - uv1;
+
+	float f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+	tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+	tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+	tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+	tangent1 = glm::normalize(tangent1);
+
+	bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+	bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+	bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+	bitangent1 = glm::normalize(bitangent1);
+
+	// triangle 2
+	// ----------
+	edge1 = pos3 - pos1;
+	edge2 = pos4 - pos1;
+	deltaUV1 = uv3 - uv1;
+	deltaUV2 = uv4 - uv1;
+
+	f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+	tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+	tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+	tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+	tangent2 = glm::normalize(tangent2);
+
+
+	bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+	bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+	bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+	bitangent2 = glm::normalize(bitangent2);
+
+
+	float quadVertices[] = {
+		// positions            // normal         // texcoords  // tangent                          // bitangent
+		pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+		pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+		pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+
+		pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+		pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+		pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
+	};
+
+
+
+
+
+	// 1. Set up an array of vectex data for a cube with index buffer data
+/*	GLfloat vertices[] = {
+		 // position		 // tex coords
+
+		// front face
+		-1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
+		 1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f, 1.0f, 1.0f,
+		-1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
+		-1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
+		 1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
+
+		// back face
+		-1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+		 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+		 1.0f,  1.0f, -1.0f, 1.0f, 1.0f,
+		-1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+
+		// left face
+		-1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+		-1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
+		-1.0f,  1.0f,  1.0f, 1.0f, 1.0f,
+		-1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		-1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
+
+		// right face
+		 1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
+		 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+		 1.0f,  1.0f, -1.0f, 1.0f, 1.0f,
+		 1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
+		 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+
+		 // top face
+		-1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+		 1.0f,  1.0f,  1.0f, 1.0f, 0.0f,
+		 1.0f,  1.0f, -1.0f, 1.0f, 1.0f,
+		-1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+		-1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f, 1.0f, 0.0f,
+
+		 // bottom face
+		-1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
+		 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+		 1.0f, -1.0f,  1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+	};   */
+
+	// Cube position
+	glm::vec3 cubePos = glm::vec3(1.5f, 0.0f, -5.0f);
+	glm::vec3 normalcubePos = glm::vec3(-1.5f, 0.0f, -5.0f);
+
+
+	// 2. Set up buffers on the GPU
+	GLuint vbo, vao;
+
+	glGenBuffers(1, &vbo);					// Generate an empty vertex buffer on the GPU
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);		// "bind" or set as the current buffer we are working with
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);	// copy the data from CPU to GPU
+
+	glGenVertexArrays(1, &vao);				// Tell OpenGL to create new Vertex Array Object
+	glBindVertexArray(vao);					// Make it the current one
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)0);
+	
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(3 * sizeof(float)));
+	
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(6 * sizeof(float)));
+	
+	glEnableVertexAttribArray(3);
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(8 * sizeof(float)));
+	
+	glEnableVertexAttribArray(4);
+	glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(11 * sizeof(float)));
+
+
+	
+
+	glBindVertexArray(0);					// unbind to make sure other code doesn't change it
+
+	ShaderProgram shaderProgram;
+	shaderProgram.loadShaders("shaders/Parallax.vert", "shaders/Parallax.frag");
+
+	ShaderProgram normalmapshader;
+	normalmapshader.loadShaders("shaders/NormalMap.vert", "shaders/NormalMap.frag");
+
+	Texture2D texture[3];
+	texture[0].loadTexture(diffuseMap, true);
+	texture[1].loadTexture(normalMap, true);
+	texture[2].loadTexture(depthMap, true);
+
+	Texture2D texture2[2];
+	texture2[0].loadTexture(diffuseMap, true);
+	texture2[1].loadTexture(normalMap, true);
+
+	double lastTime = glfwGetTime();
+	float cubeAngle = 0.0f;
+
+	// Rendering loop
+	while (!glfwWindowShouldClose(gWindow))
+	{
+		showFPS(gWindow);
+
+		double currentTime = glfwGetTime();
+		double deltaTime = currentTime - lastTime;
+
+		// Poll for and process events
+		glfwPollEvents();
+
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		texture[0].bind(0);
+		texture[1].normalbind(0);
+		texture[2].depthbind(0);
+
+		glm::mat4 model, view, projection , model2;
+
+		// Update the cube position and orientation.  Rotate first then translate
+		cubeAngle += (float)(deltaTime * 50.0f);
+		if (cubeAngle >= 360.0f) cubeAngle = 0.0f;
+
+		// Rotates around the cube center
+		model = glm::translate(model, cubePos) * glm::rotate(model, glm::radians(cubeAngle), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		// Uncomment this line and comment the one above to flip the transformation of the cube.  Watch what happens!
+		// Rotates around??
+		//model = glm::rotate(model, glm::radians(gCubeAngle), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::translate(model, cubePos);
+
+		glm::vec3 camPos(0.0f, 0.0f, 0.0f);
+		glm::vec3 targetPos(0.0f, 0.0f, -1.0f);
+		glm::vec3 up(0.0f, 1.0f, 0.0f);
+
+		// Create the View matrix
+		view = glm::lookAt(camPos, camPos + targetPos, up);
+
+		// Create the projection matrix
+		projection = glm::perspective(glm::radians(45.0f), (float)gWindowWidth / (float)gWindowHeight, 0.1f, 100.0f);
+
+		// Must be called BEFORE setting uniforms because setting uniforms is done
+		// on the currently active shader program.
+		shaderProgram.use();
+
+		// Pass the matrices to the shader
+		shaderProgram.setUniform("model", model);
+		shaderProgram.setUniform("view", view);
+		shaderProgram.setUniform("projection", projection);
+		shaderProgram.setUniform("lightPos", lightPos);
+		shaderProgram.setUniform("viewPos", camPos);
+		shaderProgram.setUniform("heightScale", heightScale);
+
+		shaderProgram.setUniform("diffuseMap", 0);
+		shaderProgram.setUniform("normalMap", 1);
+		shaderProgram.setUniform("depthMap", 2);
+
+
+
+
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		
+
+		normalmapshader.use();
+		texture2[0].bind(0);
+		texture2[1].normalbind(0);
+
+		model2 = glm::translate(model2, normalcubePos) * glm::rotate(model2, glm::radians(cubeAngle), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		normalmapshader.setUniform("model", model2);
+		normalmapshader.setUniform("view", view);
+		normalmapshader.setUniform("projection", projection);
+		normalmapshader.setUniform("lightPos", lightPos);
+		normalmapshader.setUniform("viewPos", view);
+
+		normalmapshader.setUniform("diffuseMap", 0);
+		normalmapshader.setUniform("normalMap", 1);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glBindVertexArray(0);
+
+		// Swap front and back buffers
+		glfwSwapBuffers(gWindow);
+
+		lastTime = currentTime;
+	}
+
+	// Clean up
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+	
+	glfwTerminate();
+
+	return 0;
+}
+
+
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// Initialize GLFW and OpenGL
+//-----------------------------------------------------------------------------
+bool initOpenGL()
+{
+	// Intialize GLFW 
+	// GLFW is configured.  Must be called before calling any GLFW functions
+	if (!glfwInit())
+	{
+		// An error occured
+		std::cerr << "GLFW initialization failed" << std::endl;
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);	// forward compatible with newer versions of OpenGL as they become available but not backward compatible (it will not run on devices that do not support OpenGL 3.3
+
+	// Create an OpenGL 3.3 core, forward compatible context window
+	gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+	if (gWindow == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	// Make the window's context the current one
+	glfwMakeContextCurrent(gWindow);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		return false;
+	}
+
+	// Set the required callback functions
+	glfwSetKeyCallback(gWindow, glfw_onKey);
+	glfwSetFramebufferSizeCallback(gWindow, glfw_onFramebufferSize);
+
+	glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+
+	// Define the viewport dimensions
+	glViewport(0, 0, gWindowWidth, gWindowHeight);
+	glEnable(GL_DEPTH_TEST);
+
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+// Is called whenever a key is pressed/released via GLFW
+//-----------------------------------------------------------------------------
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	
+	if (key == GLFW_KEY_F1 && action == GLFW_PRESS)
+	{
+		gWireframe = !gWireframe;
+		if (gWireframe)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		else
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+
+	if (key == GLFW_KEY_S)
+	{
+		lightPos.x = lightPos.x - 0.1f;
+	}
+	if (key == GLFW_KEY_W)
+	{
+		lightPos.x = lightPos.x + 0.1f;
+	}
+	if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+	{
+		if (heightScale > 0.0f)
+			heightScale -= 0.0005f;
+		else
+			heightScale = 0.0f;
+	}
+	if (key == GLFW_KEY_E && action == GLFW_PRESS)
+	{
+		if (heightScale < 1.0f)
+			heightScale += 0.0005f;
+		else
+			heightScale = 1.0f;
+	}
+
+}
+
+//-----------------------------------------------------------------------------
+// Is called when the window is resized
+//-----------------------------------------------------------------------------
+void glfw_onFramebufferSize(GLFWwindow* window, int width, int height)
+{
+	gWindowWidth = width;
+	gWindowHeight = height;
+	glViewport(0, 0, gWindowWidth, gWindowHeight);
+}
+
+//-----------------------------------------------------------------------------
+// Code computes the average frames per second, and also the average time it takes
+// to render one frame.  These stats are appended to the window caption bar.
+//-----------------------------------------------------------------------------
+void showFPS(GLFWwindow* window)
+{
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime(); // returns number of seconds since GLFW started, as double float
+
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	// Limit text updates to 4 times per second
+	if (elapsedSeconds > 0.25)
+	{
+		previousSeconds = currentSeconds;
+		double fps = (double)frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+
+		// The C++ way of setting the window title
+		std::ostringstream outs;
+		outs.precision(3);	// decimal places
+		outs << std::fixed
+			<< APP_TITLE << "    "
+			<< "FPS: " << fps << "    "
+			<< "Frame Time: " << msPerFrame << " (ms)";
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		// Reset for next average.
+		frameCount = 0;
+	}
+
+	frameCount++;
+}
